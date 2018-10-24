84a307a9f0321b9606bb6fdf534258306630e31c
lad4284 <lad4284@rit.edu>
2018-10-24 13:09:40 -0400

    created EdgeFieldTest and moved JUnit4 files to the lib directory

---
+++ tests/EdgeFieldTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+/**
+ * Created by Lauren on 10/24/18.
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EdgeFieldTest {
+}
