fd653e389bb78b80e0d43313223dfd85febba93d
lad4284 <lad4284@rit.edu>
2018-10-24 13:34:26 -0400

    skeleton for EdgeFieldTest

--- tests/EdgeFieldTest.java
+++ tests/EdgeFieldTest.java
@@ -1,5 +1,3 @@
-package tests;
-
 /**
  * Created by Lauren on 10/24/18.
  */
@@ -9,4 +7,149 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class EdgeFieldTest {
+    EdgeField testObj;
+
+    @Before
+    public void setUp() throws Exception {
+        testObj = new EdgeField();
+        runner();
+    }
+
+    public void runner(){
+        testGetNumFigure();
+        testGetName();
+        testGetTableID();
+        testSetTableID();
+        testGetTableBound();
+        testSetTableBound();
+        testGetFieldBound();
+        testSetFieldBound();
+        testGetDisallowNull();
+        testSetDisallowNull();
+        testGetIsPrimaryKey();
+        testSetIsPrimaryKey();
+        testGetDefaultValue();
+        testSetDefaultValue();
+        testGetVarcharValue();
+        testSetVarcharValue();
+        testGetDataType();
+        testSetDataType();
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetNumFigure(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetName(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetTableID(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetTableID(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetTableBound(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetTableBound(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetFieldBound(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetFieldBound(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetDisallowNull(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetDisallowNull(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetIsPrimaryKey(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetIsPrimaryKey(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetDefaultValue(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetDefaultValue(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetVarcharValue(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetVarcharValue(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testGetDataType(){
+
+    }
+
+    /*TODO*/
+    @Test
+    public void testSetDataType(){
+
+    }
+
+
 }
+
+
+
+
+
+
+
